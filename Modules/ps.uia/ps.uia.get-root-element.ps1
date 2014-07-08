e672a82962e243a6531589d86d5097307f3b2b16
xiongjia <lexiongjia@gmail.com>
2014-07-09 00:06:17 +0800

    implment the new get uia root element

--- Modules/ps.uia/ps.uia.get-root-element.ps1
+++ Modules/ps.uia/ps.uia.get-root-element.ps1
@@ -4,6 +4,45 @@
 
 Function Get-UIARootElement
 {
-    Write-Output "My test"
+    [CmdletBinding(
+    	SupportsShouldProcess=$true,
+        ConfirmImpact="Low"
+    )]
+    Param(
+        [Parameter(Mandatory=$true)]
+        [Int]$ProcessId
+    )
+
+    Begin
+    {
+        Write-Debug "Begin Get-UIARootElement"
+        Write-Debug "ProcessId= $ProcessId"
+    }
+
+	Process
+    {
+        $rootElement = [Windows.Automation.AutomationElement]::RootElement
+        $condAUTProc = New-Object Windows.Automation.PropertyCondition(
+            [Windows.Automation.AutomationElement]::ProcessIdProperty,
+            $ProcessId
+        )
+        $autElement = $rootElement.FindFirst(
+            [Windows.Automation.TreeScope]::Children,
+            $condAUTProc
+        )
+
+        if ($autElement -eq $null)
+        {
+            Write-Error -Message "Cannot find the UIA Element for process ($ProcessId)"
+        }
+
+        $rootElement = @{
+            "ProcessId"=$ProcessId;
+            "RootElement"=[ref]$autElement
+        }
+        Return $rootElement
+    }
+
+    End {}
 }
 
